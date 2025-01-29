1a7712bc2138f56dcb2fde09d63ed9f1f77ca2d9
a-know <a.know.dev@gmail.com>
2025-01-29 16:23:55 +0900

    Add view

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,8 @@ class HelloController < ApplicationController
   def index
     render plain: 'Hello, World!'
   end
+
+  def view
+    @msg = 'Hello, World!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get 'hello/index', to: 'hello#index'
+  get 'hello/view'
 end
